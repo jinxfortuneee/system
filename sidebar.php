b19b032bae8c3421a99d2f5bfaea1a74869c60e2
jinxfortuneee <107634914+jinxfortuneee@users.noreply.github.com>
2024-04-16 23:12:21 +0800

    sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -32,6 +32,57 @@ $school_year = $s['school_year'];
         </ul>
     </li>
 
+    <li>
+        <a href="javascript:void(0)" data-toggle="collapse" data-target="#recordsCollapse"><i
+                class="fa fa-fw fa-file"></i> RECORDS <i class="fa fa-fw fa-caret-down"></i></a>
+        <ul class="collapse" id="recordsCollapse">
+            <li>
+                <a href="rms.php?page=records"><i class="fa fa-fw fa-files-o"></i>Academic Record </a>
+            </li>
+            <li>
+                <a href="rms.php?page=candidates&sy=<?php echo $school_year ?>"><i class="fa fa-fw fa-users"></i>
+                    Graduates candidates</a>
+            </li>
+            <li>
+                <a href="rms.php?page=candidates_list&sy=<?php echo $school_year ?>"><i class="fa fa-fw fa-file-o"></i>
+                    Graduates List </a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="javascript:void(0)" data-toggle="collapse" data-target="#reportsCollapse"><i
+                class="fa fa-fw fa-file"></i> OUTPUTS <i class="fa fa-fw fa-caret-down"></i></a>
+        <ul id="reportsCollapse" class="collapse">
+            <li>
+                <a href="rms.php?page=report"><i class=""></i> Grades</a>
+            </li>
+            <li>
+                <a href="#" data-toggle="modal" data-target="#s_report"><i class=""></i> Students List</a>
+            </li>
+            <li>
+                <a href="rms.php?page=candidates_report&school_year=<?php echo $school_year ?>"><i class=""></i>
+                    Graduates report</a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="javascript:void(0)" data-toggle="collapse" data-target="#settingsCollapse"><i
+                class="fa fa-fw fa-file"></i> SETTINGS <i class="fa fa-fw fa-caret-down"></i></a>
+        <ul id="settingsCollapse" class="collapse">
+            <li>
+                <a href="rms.php?page=users"><i class="fa fa-users"></i> Users</a>
+            </li>
+            <li>
+                <a href="rms.php?page=school_year"><i class="fa fa-fw fa-calendar"></i>School Year</a>
+            </li>
+            <li>
+                <a href="rms.php?page=grade"><i class="fa fa-fw fa-file-text-o"></i> Year List</a>
+            </li>
+            <li>
+                <a href="#" data-toggle="modal" data-target="#ss_report"><i class=""></i> Archive</a>
+            </li>
+        </ul>
+    </li>
 </ul>
 <script>
     $('.side-nav li a').each(function () {
